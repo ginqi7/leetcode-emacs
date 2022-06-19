a946085ac0da260480771a41e06fb966d8e669a3
JinQiqi <jinqiqi@luren-ali-mac.local>
2022-06-19 16:25:25 +0800

    feat: add click hook and list filter

--- leetcode.el
+++ leetcode.el
@@ -2,11 +2,13 @@
 ;;; Commentary:
 ;;; code:
 (require 'ctable)
-
+(require 'cl-lib)
 
 (defvar leetcode-path "~/.leetcode/code")
 
 (defvar leetcode-language "go")
+
+(defvar leetcode-hide-no-auth-problems t)
 
 (defun leetcode--buffer-whole-string (buffer)
   "Get String without properties from other buffer"
@@ -21,46 +23,76 @@
 
 (defun get-file-name-num (name)
   "Convert file name to get first number"
-  (string-to-number (first (split-string name "\\.")))
-  )
+  (string-to-number (first (split-string name "\\."))))
 
-(defun leetcode--split-string (str)
+
+(defun leetcode--parse-leetcode-entry (str)
   "Divide a leetcode entry title into 5 columns.
 STR is a leetcode entry title."
-  (let* ((n1 (string-match "\\[" str))
-         (n2 (string-match "\\]" str n1))
-         (n3 (string-match "Easy\\|Medium\\|Hard" str n1))
-         (n4 (string-match "\(" str n3))
-         (substr1 (substring str 0 n1))
-         (substr2 (substring str n1 (+ 1 n2)))
-         (substr3 (substring str (+ 1 n2) n3))
-         (substr4 (substring str n3 n4))
-         (substr5 (substring str n4)))
-    (list substr1 substr2  substr3 substr4 substr5)))
+  (let ((the-list (split-string str " +\\|\\[\\|\\]" t))
+        (accepted)
+        (number)
+        (title)
+        (difficulty)
+        (frequency))
+    (if (string-match "[0-9]+" (car the-list))
+        (setq number (car the-list)
+              title (mapconcat 'identity (cl-subseq the-list 1 (- (length the-list) 3)) " "))
+      (setq acceped (car the-list)
+          number (nth 1 the-list)
+              title (mapconcat 'identity (cl-subseq the-list 2 (- (length the-list) 3)) " ")))
+    (setq difficulty (nth (- (length the-list) 3) the-list)
+          frequency (concat (nth (- (length the-list) 2) the-list) (nth (- (length the-list) 1) the-list)))
+    
+    (list acceped number title difficulty frequency)))
 
+(defun leetcode--entry-filter (lst)
+  (if leetcode-hide-no-auth-problems
+      (not (string= (car lst) "🔒"))
+    t))
+
+(defun leetcode--parse-leetcode-list (str)
+  "Divide a leetcode entry title into 5 columns.
+STR is a leetcode entry title."
+  (seq-filter 
+   #'leetcode--entry-filter
+   (mapcar #'leetcode--parse-leetcode-entry (split-string str "\n+" t))))
+
+(defun leetcode--create-cmodel (title)
+  "Create cmodel by title string"
+  (make-ctbl:cmodel :title  title
+                    :align 'left))
+
+(defun leetcode-add-click-hook ()
+  (goto-line 0)
+  (lexical-let ((cp (ctbl:cp-get-component)))
+    (ctbl:cp-add-click-hook 
+     cp 
+     (lambda ()
+       (leetcode-show (string-to-number (nth 1 (ctbl:cp-get-selected-data-row cp))))))))
 
 (defun leetcode--list-all-sync (process signal)
   "Create a new buffer to show all leetcode programs list."
   (when (memq (process-status process) '(exit signal))
     (let* ((mode-buffer (get-buffer-create "*leetcode-list*"))
-           (show-list (reverse (split-string (substring-no-properties (ansi-color-apply (leetcode--buffer-whole-string "leetcode_list_value")))   "\n")))
-           (rows  (mapcar 'leetcode--split-string (remove-if (lambda (arg) (string= "" arg)) show-list)))
+           (rows (leetcode--parse-leetcode-list (leetcode--buffer-whole-string "leetcode_list_value")))
            (async-model ; wrapping a large data in async-data-model
             (ctbl:async-model-wrapper rows)))
+
       (kill-buffer "leetcode_list_value")
       (switch-to-buffer mode-buffer)
       (erase-buffer)
-      (pop show-list)
       (ctbl:create-table-component-region
        :model
        (make-ctbl:model
         :column-model
-        (list (make-ctbl:cmodel :title "Accepted")
-              (make-ctbl:cmodel :title "Number")
-              (make-ctbl:cmodel :title "Title")
-              (make-ctbl:cmodel :title "Difficulty")
-              (make-ctbl:cmodel :title "Frequency"))
+        (mapcar 'leetcode--create-cmodel (list "Accepted"
+                                          "Number"
+                                          "Title"
+                                          "Difficulty"
+                                          "Frequency"))
         :data async-model)))
+    (leetcode-add-click-hook)
     (setq buffer-read-only nil)
     (goto-char (point-min))))
 
@@ -78,7 +110,6 @@ STR is a leetcode entry title."
     (with-current-buffer (process-buffer proc)
       (insert (ansi-color-apply string)))))
 
-
 (defun leetcode--exec (action num)
   "Execute leetcode action"
   (let ((mode-buffer (get-buffer-create "*leetcode-result*")))
@@ -89,7 +120,7 @@ STR is a leetcode entry title."
     (erase-buffer)
     (other-window 1)) 
   
-  (make-process :name "leetcode list"
+  (make-process :name "leetcode exec"
                 :buffer "*leetcode-result*"
                 :command `("leetcode" ,action ,num)
                 :filter #'leetcode--ansi-color-insertion-filter))
@@ -98,8 +129,9 @@ STR is a leetcode entry title."
   "Async Create a new buffer to show all leetcode programs list."
   (interactive)
   (make-process :name "leetcode list"
-                :buffer "leetcode_list_value" 
+                :buffer "leetcode_list_value"
                 :command '("leetcode" "list")
+                :filter #'leetcode--ansi-color-insertion-filter
                 :sentinel 'leetcode--list-all-sync))
 
 (defun leetcode-show (n)
