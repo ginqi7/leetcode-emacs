08173215d4a0b62ed6f79db0c63dd76b8226e664
qiqi.jin <qiqi.jin@alibaba-inc.com>
2023-08-04 11:05:00 +0800

    feat: add some functions for selecting top 100 and selecting randomly.

--- leetcode.el
+++ leetcode.el
@@ -33,6 +33,8 @@
 
 (defvar leetcode-hide-no-auth-problems t)
 
+(defvar leetcode-top100 '("1. Two Sum" "2. Add Two Numbers" "3. Longest Substring Without Repeating Characters" "4. Median of Two Sorted Arrays" "5. Longest Palindromic Substring" "11. Container With Most Water" "13. Roman to Integer" "14. Longest Common Prefix" "15. 3Sum" "17. Letter Combinations of a Phone Number" "19. Remove Nth Node From End of List" "20. Valid Parentheses" "21. Merge Two Sorted Lists" "22. Generate Parentheses" "23. Merge k Sorted Lists" "24. Swap Nodes in Pairs" "25. Reverse Nodes in k-Group" "31. Next Permutation" "32. Longest Valid Parentheses" "33. Search in Rotated Sorted Array" "34. Find First and Last Position of Element in Sorted Array" "35. Search Insert Position" "39. Combination Sum" "41. First Missing Positive" "42. Trapping Rain Water" "45. Jump Game II" "46. Permutations" "48. Rotate Image" "49. Group Anagrams" "51. N-Queens" "53. Maximum Subarray" "54. Spiral Matrix" "55. Jump Game" "56. Merge Intervals" "62. Unique Paths" "64. Minimum Path Sum" "70. Climbing Stairs" "72. Edit Distance" "73. Set Matrix Zeroes" "74. Search a 2D Matrix" "75. Sort Colors" "76. Minimum Window Substring" "78. Subsets" "79. Word Search" "84. Largest Rectangle in Histogram" "94. Binary Tree Inorder Traversal" "98. Validate Binary Search Tree" "101. Symmetric Tree" "102. Binary Tree Level Order Traversal" "104. Maximum Depth of Binary Tree" "105. Construct Binary Tree from Preorder and Inorder Traversal" "114. Flatten Binary Tree to Linked List" "118. Pascal's Triangle" "121. Best Time to Buy and Sell Stock" "124. Binary Tree Maximum Path Sum" "128. Longest Consecutive Sequence" "131. Palindrome Partitioning" "136. Single Number" "138. Copy List with Random Pointer" "139. Word Break" "141. Linked List Cycle" "142. Linked List Cycle II" "146. LRU Cache" "148. Sort List" "152. Maximum Product Subarray" "153. Find Minimum in Rotated Sorted Array" "155. Min Stack" "160. Intersection of Two Linked Lists" "169. Majority Element" "189. Rotate Array" "198. House Robber" "199. Binary Tree Right Side View" "200. Number of Islands" "206. Reverse Linked List" "207. Course Schedule" "208. Implement Trie (Prefix Tree)" "215. Kth Largest Element in an Array" "226. Invert Binary Tree" "230. Kth Smallest Element in a BST" "234. Palindrome Linked List" "236. Lowest Common Ancestor of a Binary Tree" "238. Product of Array Except Self" "239. Sliding Window Maximum" "240. Search a 2D Matrix II" "283. Move Zeroes" "287. Find the Duplicate Number" "295. Find Median from Data Stream" "300. Longest Increasing Subsequence" "322. Coin Change" "347. Top K Frequent Elements" "394. Decode String" "416. Partition Equal Subset Sum" "438. Find All Anagrams in a String" "543. Diameter of Binary Tree" "560. Subarray Sum Equals K" "567. Permutation in String" "704. Binary Search" "739. Daily Temperatures" "994. Rotting Oranges" "1143. Longest Common Subsequence"))
+
 (defun leetcode--buffer-whole-string (buffer)
   "Get String without properties from other buffer.
 BUFFER is the buffer name"
@@ -202,6 +204,31 @@ NUM is leetcode problem's number."
                 :sentinel 'leetcode--list-all-sync))
 
 
+(defun leetcode--random (process signal)
+  "Random open a leetcode problem.
+PROCESS is current running process.
+SIGNAL is current running process' signal."
+  (when (memq (process-status process) '(exit signal))
+    (let* ((mode-buffer (get-buffer-create "*leetcode-list*"))
+           (rows
+            (leetcode--parse-leetcode-list
+             (process-get process 'output)))
+           (candidates
+            (mapcar
+             (lambda (row)
+               (format "%s: %s (%s)"
+                       (second row)
+                       (third row)
+                       (nth 3 row)))
+             rows))
+           (selected-item
+            (nth (random (length candidates)) candidates))
+           (seleted-num (car (split-string selected-item ":"))))
+      (leetcode-show (string-to-number seleted-num))
+      (kill-buffer "leetcode_list_value"))))
+
+
+
 (defun leetcode--list-all-interactive (process signal)
   "List all leetcode in minibuffer.
 PROCESS is current running process.
@@ -234,6 +261,15 @@ SIGNAL is current running process' signal."
                 :filter #'leetcode--ansi-color-insertion-filter
                 :sentinel 'leetcode--list-all-interactive))
 
+(defun leetcode-random ()
+  "Random show a leetcode question."
+  (interactive)
+  (make-process :name "leetcode list"
+                :buffer "leetcode_list_value"
+                :command '("leetcode" "list")
+                :filter #'leetcode--ansi-color-insertion-filter
+                :sentinel 'leetcode--random))
+
 
 (defun leetcode-filter-by-difficulty ()
   "Async Create a new buffer to query leetcode programs list by difficulty."
@@ -247,6 +283,20 @@ SIGNAL is current running process' signal."
                   :command (list "leetcode" "list" "-q" option)
                   :filter #'leetcode--ansi-color-insertion-filter
                   :sentinel 'leetcode--list-all-sync)))
+
+(defun leetcode-random-by-difficulty ()
+  "Select a difficulty and random show a leetcode question."
+  (interactive)
+  (let* ((difficulty
+          (completing-read "Select Leetcode Difficulty: "
+                           '("All" "Easy" "Medium" "Hard")))
+         (option (downcase (substring difficulty 0 1))))
+    (make-process :name "leetcode list"
+                  :buffer "leetcode_list_value"
+                  :command (list "leetcode" "list" "-q" option)
+                  :filter #'leetcode--ansi-color-insertion-filter
+                  :sentinel 'leetcode-random)))
+
 
 (defun leetcode-filter-by-difficulty-interactive ()
   "Async interactively select leetcode programs by difficulty and show it."
@@ -273,6 +323,19 @@ SIGNAL is current running process' signal."
                   :command (list "leetcode" "list" "-t" option)
                   :filter #'leetcode--ansi-color-insertion-filter
                   :sentinel 'leetcode--list-all-sync)))
+
+(defun leetcode-random-by-tag()
+  "Select a tag and show a question randomly."
+  (interactive)
+  (let* ((tag
+          (completing-read "Select Leetcode tag: "
+                           '("Array" "String" "Hash Table" "Dynamic Programming" "Math" "Sorting" "Depth-First Search" "Greedy" "Database" "Breadth-First Search" "Tree" "Binary Search" "Matrix" "Binary Tree" "Two Pointers" "Bit Manipulation" "Stack" "Design" "Heap (Priority Queue)" "Graph" "Simulation" "Backtracking" "Prefix Sum" "Counting" "Sliding Window" "Linked List" "Union Find" "Monotonic Stack" "Ordered Set" "Recursion" "Trie" "Binary Search Tree" "Enumeration" "Divide and Conquer" "Bitmask" "Queue" "Memoization" "Geometry" "Topological Sort" "Segment Tree" "Game Theory" "Hash Function" "Binary Indexed Tree" "Interactive")))
+         (option (downcase (string-replace " " "-" tag))))
+    (make-process :name "leetcode list"
+                  :buffer "leetcode_list_value"
+                  :command (list "leetcode" "list" "-t" option)
+                  :filter #'leetcode--ansi-color-insertion-filter
+                  :sentinel 'leetcode--random)))
 
 (defun leetcode-filter-by-tag-interactive()
   "Async interactively select leetcode programs by tag and show it."
@@ -350,6 +413,28 @@ N is a leetcode program number."
       (leetcode-filter-by-difficulty-interactive))
      ((string= "query by tag" selected-item)
       (leetcode-filter-by-tag-interactive)))))
+
+(defun leetcode-top100 ()
+  "Leetcode top 100."
+  (interactive)
+  (let* ((leetcode-question
+          (completing-read "Select a problem: " leetcode-top100))
+         (leetcode-num
+          (car (string-split leetcode-question "\\." t " "))))
+    (print leetcode-question)
+    (leetcode-show (string-to-number leetcode-num)))
+  )
+
+(defun leetcode-top100-random ()
+  "Leetcode top 100 random."
+  (interactive)
+  (let* ((leetcode-question
+          (nth (random 100) leetcode-top100))
+         (leetcode-num
+          (car (string-split leetcode-question "\\." t " "))))
+    (print leetcode-question)
+    (leetcode-show (string-to-number leetcode-num)))
+  )
 
 (provide 'leetcode)
 ;;; leetcode.el ends here
